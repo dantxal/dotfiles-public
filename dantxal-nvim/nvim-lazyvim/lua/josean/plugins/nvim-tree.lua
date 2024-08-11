b5a4497d7233d7e2575d44b24584738c44bd29f8
dantxal <dantxal@gmail.com>
2024-08-11 17:50:10 -0300

    nvim feat: make nvim-tree selected file follow current buffer

--- dantxal-nvim/nvim-lazyvim/lua/josean/plugins/nvim-tree.lua
+++ dantxal-nvim/nvim-lazyvim/lua/josean/plugins/nvim-tree.lua
@@ -9,6 +9,9 @@ return {
     vim.g.loaded_netrwPlugin = 1
 
     nvimtree.setup({
+      update_focused_file = {
+        enable = true,
+      },
       view = {
         width = 35,
         relativenumber = true,
@@ -52,5 +55,5 @@ return {
     keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
     keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
     keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
-  end
+  end,
 }
