692ee94a216a271a783bc4c855dee87302d77791
dantxal <dantxal@gmail.com>
2024-08-11 17:52:35 -0300

    tmux fix: replace keybind by tmux-navigator

--- tmux/tmux.conf.sh
+++ tmux/tmux.conf.sh
@@ -67,8 +67,8 @@ unbind L
 bind J select-layout even-horizontal
 bind K select-layout even-vertical
 bind L select-layout tiled
-bind C-j select-layout main-horizontal
-bind C-k select-layout main-vertical
+# bind C-j select-layout main-horizontal
+# bind C-k select-layout main-vertical
 
 # Reload the tmux configuration, display a 2 second message
 unbind r
@@ -178,7 +178,26 @@ set -g @plugin 'tmux-plugins/tpm'
 
 # list of tmux plugins
 # for navigating panes and vim/nvim with Ctrl-hjkl
-# set -g @plugin 'christoomey/vim-tmux-navigator'
+set -g @plugin 'christoomey/vim-tmux-navigator'
+# Smart pane switching with awareness of Vim splits.
+# See: https://github.com/christoomey/vim-tmux-navigator
+is_vim="ps -o state= -o comm= -t '#{pane_tty}' \
+    | grep -iqE '^[^TXZ ]+ +(\\S+\\/)?g?(view|l?n?vim?x?|fzf)(diff)?$'"
+bind-key -n 'C-h' if-shell "$is_vim" 'send-keys C-h'  'select-pane -L'
+bind-key -n 'C-j' if-shell "$is_vim" 'send-keys C-j'  'select-pane -D'
+bind-key -n 'C-k' if-shell "$is_vim" 'send-keys C-k'  'select-pane -U'
+bind-key -n 'C-l' if-shell "$is_vim" 'send-keys C-l'  'select-pane -R'
+tmux_version='$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")'
+if-shell -b '[ "$(echo "$tmux_version < 3.0" | bc)" = 1 ]' \
+    "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\'  'select-pane -l'"
+if-shell -b '[ "$(echo "$tmux_version >= 3.0" | bc)" = 1 ]' \
+    "bind-key -n 'C-\\' if-shell \"$is_vim\" 'send-keys C-\\\\'  'select-pane -l'"
+
+bind-key -T copy-mode-vi 'C-h' select-pane -L
+bind-key -T copy-mode-vi 'C-j' select-pane -D
+bind-key -T copy-mode-vi 'C-k' select-pane -U
+bind-key -T copy-mode-vi 'C-l' select-pane -R
+bind-key -T copy-mode-vi 'C-\' select-pane -l
 
 # Powerline theme
 # set -g @plugin 'jimeh/tmux-themepack'
