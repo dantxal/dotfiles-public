f460cf804d055e5c71c0fec86ceaa1d88ad222b2
Daniel Faria <dantxal@gmail.com>
2025-09-30 15:28:52 -0300

    feat(copilot-chat): remove clear command since it conflicts with switching panes and vanishes history

--- dantxal-nvim/nvim-lazyvim/lua/josean/plugins/copilot-chat.lua
+++ dantxal-nvim/nvim-lazyvim/lua/josean/plugins/copilot-chat.lua
@@ -6,13 +6,18 @@ return {
       { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
     },
     build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      -- See Configuration section for options
-      mappings = {
-        -- Disable the default <C-l> mapping if it exists
-        ["<C-l> "] = false,
-      },
-    },
-    -- See Commands section for default commands if you want to lazy load on them
+    opts = function(_, opts)
+      local CopilotChat = require("CopilotChat")
+
+      -- copy defaults so we don’t mutate the global config directly
+      local commands = vim.deepcopy(CopilotChat.config.defaults.commands)
+
+      -- remove "clear" command, because it's annoying, history vanishes
+      commands.clear = nil
+
+      opts.commands = commands
+
+      return opts
+    end,
   },
 }
