7f8f8a089aa21c93acb827778024bf803c942c8f
Daniel Faria <dantxal@gmail.com>
2025-09-30 20:50:05 -0300

    feat(formatting): add FormatDisable and FormatEnable commands for autoformat toggle

--- dantxal-nvim/nvim-lazyvim/lua/josean/plugins/formatting.lua
+++ dantxal-nvim/nvim-lazyvim/lua/josean/plugins/formatting.lua
@@ -21,13 +21,40 @@ return {
         lua = { "stylua" },
         python = { "isort", "black" },
       },
-      format_on_save = {
-        lsp_fallback = true,
-        async = false,
-        timeout_ms = 1000,
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
     })
 
+    -- Commands to toggle autoformat-on-save
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+
+      vim.notify = require("notify")
+      vim.notify("autoformat-on-save disabled globally", 0, { title = "conform.nvim" })
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+
+      vim.notify = require("notify")
+      vim.notify("autoformat-on-save enabled globally", 0, { title = "conform.nvim" })
+    end, {
+      desc = "Re-enable autoformat-on-save",
+    })
+
+    -- Format file or selection
     vim.keymap.set({ "n", "v" }, "<leader>mp", function()
       conform.format({
         lsp_fallback = true,
